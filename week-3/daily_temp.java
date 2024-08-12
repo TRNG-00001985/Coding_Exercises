cbe9b36fe671b06c61ae49cd7acf3647f9506acc
Suneel Kumar <karrisuneelkumar2003@gmail.com>
2024-08-12 21:44:20 +0530

    week-3 dsa codes

---
+++ week-3/daily_temp.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class daily_temp{
+    public int[] dailyTemperatures(int[] temps) {
+        int[] results = new int[temps.length];
+        Stack<Integer> stack = new Stack<>();
+        /// UPVOTE !
+        for (int i = 0; i < temps.length; i++) {
+            while (!stack.isEmpty() && temps[stack.peek()] < temps[i]) {
+                results[stack.peek()] = i - stack.pop();
+            }
+            stack.push(i);
+        }
+
+        return results;
+    }
+}
